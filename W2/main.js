ac84d9d560ce6fbfe329d88a3230b1067f549f5f
QuyV8 <Quy.vu0992@gmail.com>
2024-08-21 20:01:57 -0700

    coding is done

---
+++ W2/main.js
@@ -0,0 +1,24 @@
+// How to Calulate Your Carb Footprint - https://www.wikihow.com/Calculate-Your-Carbon-Footprint
+// Method 1
+
+// 1. Count the members of your household.
+const myHouseMembers = 6;
+// 2. Consider the size of your home
+const MyHomeSize = 7;
+// 3. Evaluate your food choices
+const MyFoodathome = 8;
+// 4. Examine your water comsumption
+const RunningWater = 3;
+// 5. How many household purchases you make each year.
+const NewHomeItems =8;
+// 6. how much waste do you produce 
+const HomeWaste = 40;
+// 7. amount of waste that you recycle 
+const HomeRecycle = 18;
+// 8. annual transportation score.
+const transportation = 28;
+// 9. Add all points
+const CFPTotal =myHouseMembers + MyHomeSize + MyFoodathome + RunningWater + NewHomeItems + HomeWaste + HomeRecycle + transportation 
+
+// 10.Total points 
+
