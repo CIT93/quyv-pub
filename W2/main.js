9e608dd5fdc578569a1731aab8313e12c78c6723
QuyV8 <Quy.vu0992@gmail.com>
2024-08-22 19:58:18 -0700

    adding main.js

--- W2/main.js
+++ W2/main.js
@@ -4,21 +4,21 @@
 // 1. Count the members of your household.
 const myHouseMembers = 6;
 // 2. Consider the size of your home
-const MyHomeSize = 7;
+const myHomeSize = 7;
 // 3. Evaluate your food choices
-const MyFoodathome = 8;
+const myFoodathome = 8;
 // 4. Examine your water comsumption
-const RunningWater = 3;
+const runningWater = 3;
 // 5. How many household purchases you make each year.
-const NewHomeItems =8;
+const newHomeItems =8;
 // 6. how much waste do you produce 
-const HomeWaste = 40;
+const homeWaste = 40;
 // 7. amount of waste that you recycle 
-const HomeRecycle = 18;
+const homeRecycle = 18;
 // 8. annual transportation score.
 const transportation = 28;
 // 9. Add all points
 const CFPTotal =myHouseMembers + MyHomeSize + MyFoodathome + RunningWater + NewHomeItems + HomeWaste + HomeRecycle + transportation 
 
-// 10.Total points 
+console. log (myHeading.textContent)
 
