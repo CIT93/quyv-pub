0324ba522d76fc4f9cb0fb978bb3c36016c02a20
QuyV8 <Quy.vu0992@gmail.com>
2024-08-27 20:29:38 -0700

    functions

---
+++ w3/main.js
@@ -0,0 +1,29 @@
+function determineHouseHoldPoints(numberInhousehold) {
+  console.log("Inside the fuction");
+  if (numberInhousehold === 1) {
+    cardonFootprintPoints = cardonFootprintPoints + 14;
+  } else if (numberInhousehold === 2) {
+    cardonFootprintPoints = cardonFootprintPoints + 12;
+  } else if (numberInhousehold === 3) {
+    cardonFootprintPoints = cardonFootprintPoints + 10;
+  } else if (numberInhousehold === 4) {
+    cardonFootprintPoints = cardonFootprintPoints + 8;
+  } else if (numberInhousehold === 5) {
+    cardonFootprintPoints = cardonFootprintPoints + 6;
+  } else if (numberInhousehold === 6) {
+    cardonFootprintPoints = cardonFootprintPoints + 4;
+  } else if (numberInhousehold > 6) {
+    cardonFootprintPoints = cardonFootprintPoints + 2;
+  }
+
+  console.log(
+    `Base on the number of member of the household of ${numberInhousehold} the points would be ${cardonFootprintPoints}.`
+  );
+}
+
+let cardonFootprintPoints = 0;
+const numberInhousehold = 3;
+
+determineHouseHoldPoints(3);
+determineHouseHoldPoints(4);
+
